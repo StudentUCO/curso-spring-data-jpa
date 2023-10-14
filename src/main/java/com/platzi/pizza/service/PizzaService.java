03636ef04c3dae779d1aaeb997950c8b3182d8a4
StudentUCO <brayan.sepulveda4302@soyuco.edu.co>
2023-10-13 22:28:42 -0500

    Se expone servicio para obteenr una pizza por su id, ademas de que se utiliza el ListCrudRepository en vez del JdbcTemplate para recuperar la informacion de la BD

--- src/main/java/com/platzi/pizza/service/PizzaService.java
+++ src/main/java/com/platzi/pizza/service/PizzaService.java
@@ -1,6 +1,7 @@
 package com.platzi.pizza.service;
 
 import com.platzi.pizza.persistence.entity.PizzaEntity;
+import com.platzi.pizza.persistence.repository.PizzaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,8 +13,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PizzaService {
     private final JdbcTemplate jdbcTemplate;
+    private final PizzaRepository pizzaRepository;
+
+    public List<PizzaEntity> getAllWithJdbcTemplate() {
+        return this.jdbcTemplate.query("SELECT * FROM pizza", new BeanPropertyRowMapper<>(PizzaEntity.class));
+    }
 
     public List<PizzaEntity> getAll() {
-        return this.jdbcTemplate.query("SELECT * FROM pizza", new BeanPropertyRowMapper<>(PizzaEntity.class));
+        return this.pizzaRepository.findAll();
+    }
+
+    public PizzaEntity get(int idPizza) {
+        return this.pizzaRepository.findById(idPizza).orElse(null);
     }
 }
