b9b4c74371d4de0217156a198168c7366fcf7366
StudentUCO <brayan.sepulveda4302@soyuco.edu.co>
2023-10-15 01:28:50 -0500

    Se configuran autenticacion basica con usuarios cargados en memoria y manejo de roles en las url

--- src/main/java/com/platzi/pizza/web/config/WebSecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/WebSecurityConfig.java
@@ -7,22 +7,56 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    public static final String ADMIN = "ADMIN";
+    public static final String CUSTOMER = "CUSTOMER";
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http, CorsConfig corsConfig) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers(HttpMethod.GET, "/api/pizzas/**").permitAll()
-                        .requestMatchers(HttpMethod.PUT).denyAll()
+                        .requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole(ADMIN, CUSTOMER)
+                        .requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole(ADMIN)
+                        .requestMatchers(HttpMethod.PUT).hasRole(ADMIN)
+                        .requestMatchers("/api/orders/**").hasRole(ADMIN)
                         .anyRequest()
                         .authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService memoryUsers() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles(ADMIN)
+                .build();
+
+        UserDetails customer = User.builder()
+                .username("customer")
+                .password(passwordEncoder().encode("customer123"))
+                .roles(CUSTOMER)
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, customer);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
