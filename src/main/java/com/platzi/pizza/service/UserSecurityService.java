de5f22fd726c280e1bc8285d844bf8156362f272
StudentUCO <brayan.sepulveda4302@soyuco.edu.co>
2023-10-17 14:31:57 -0500

    Se configuran autenticacion basica con usuarios cargados desde base de datos y se recuperan los roles tambien desde BD

---
+++ src/main/java/com/platzi/pizza/service/UserSecurityService.java
@@ -0,0 +1,32 @@
+package com.platzi.pizza.service;
+
+import com.platzi.pizza.persistence.entity.UserEntity;
+import com.platzi.pizza.persistence.entity.UserRoleEntity;
+import com.platzi.pizza.persistence.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserSecurityService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = this.userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found."));
+        String[] roles = userEntity.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
+
+        return User.builder()
+                .username(userEntity.getUsername())
+                .password(userEntity.getPassword())
+                .roles(roles)
+                .accountLocked(userEntity.getLocked())
+                .disabled(userEntity.getDisabled())
+                .build();
+    }
+}
--- src/main/java/com/platzi/pizza/web/config/WebSecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/WebSecurityConfig.java
@@ -7,12 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -38,7 +34,7 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Bean
+/*    @Bean
     public UserDetailsService memoryUsers() {
         UserDetails admin = User.builder()
                 .username("admin")
@@ -53,7 +49,7 @@ public class WebSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(admin, customer);
-    }
+    }*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
