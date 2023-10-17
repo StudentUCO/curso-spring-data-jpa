8e213e4a58fbaa1d8277d4f39c20f82c6fe2f43a
StudentUCO <brayan.sepulveda4302@soyuco.edu.co>
2023-10-17 17:49:32 -0500

    Se configura filtro de segurtidad con jwt personalizado para spring security asi nuestras APIs seran STATELESS (sin estado). Hasta este punto ya esta la seguridad debidamente configurada para un proyecto

--- src/main/java/com/platzi/pizza/web/config/WebSecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/WebSecurityConfig.java
@@ -1,32 +1,39 @@
 package com.platzi.pizza.web.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
     public static final String ADMIN = "ADMIN";
     public static final String CUSTOMER = "CUSTOMER";
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http, CorsConfig corsConfig) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/customers/**").hasAnyRole(ADMIN, CUSTOMER)
@@ -37,7 +44,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/orders/**").hasRole(ADMIN)
                         .anyRequest()
                         .authenticated())
-                .httpBasic(Customizer.withDefaults());
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
